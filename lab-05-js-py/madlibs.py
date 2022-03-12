8e49f7885d3f87322583a6e64d3da0bdcd6a06dc
JonathanO9 <Jonathan.Oladimeji@mail.citytech.cuny.edu>
2022-03-12 17:25:04 -0500

    Lab part 2 with python

---
+++ lab-05-js-py/madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Enter a noun");
+adjective = input("Enter adjective");
+adverb = input("Enter adverb");
+verb = input("Enter verb");
+
+print("On a tropical " + str(noun) + ",");
+print("Underneath a "+str(adjective)+ " lava moon");
+print("Hangin' " + str(adverb) + "with the hulu dancers");
+print(str(verb)+ " question cause' they all got the answers");
